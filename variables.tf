518aca217d33ca0e5110b103ed22c4afefa1b652
Dnyaneshwar Popatrao Kalmode <dkalmode@gmail.com>
2023-01-19 17:57:52 -0500

    added profile to run from local

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type = string
   default = "us-east-2"
 }
+
+variable "profile" {
+  description = "profile to be used to create resources."
+  type        = string
+  default     = "Terraform"
+}
